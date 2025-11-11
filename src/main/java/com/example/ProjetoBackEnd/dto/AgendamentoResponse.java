3a54293b064fc71585d35190430eedfd52243537
João Nemezio <joaogabrieldl30@gmail.com>
2025-11-10 21:04:17 -0300

    Implementando DTOs e corrigindo nome dos arquivos do wlad

--- src/main/java/com/example/ProjetoBackEnd/dto/AgendamentoResponse.java
+++ src/main/java/com/example/ProjetoBackEnd/dto/AgendamentoResponse.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
-@NoArgsConstructorg
+@NoArgsConstructor
 public class AgendamentoResponse {
     private LocalDateTime dataInicio;
     private LocalDateTime dataFim;
